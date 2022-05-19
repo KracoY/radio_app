bd719e7d25ef3f5e33422d7c61bfc551ef9e2314
KracoY <enderyuruk9932@gmail.com>
2022-05-19 17:09:32 +0200

    update

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import "./Stylesheets/player.scss";
-//import ReactAudioPlayer from "react-audio-player";
+import ReactAudioPlayer from "react-audio-player";
 
 function Player(props) {
   return (
     <div className="player">
       {/* <ReactAudioPlayer src={props.url}  controls /> */}
       <audio src={props.url} controls autoPlay></audio>
+
+      <p>BY <a href="https://ender-yuruk.com/" target="_blank">Ender Yürük</a></p>
     </div>
   );
 }
